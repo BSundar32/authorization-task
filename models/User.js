86020ef95fe7fcdaa614a064376d2de4fc2ce667
BSundar32 <sundarb3898@gmail.com>
2026-03-24 20:19:40 +0530

    fixed login issue

--- models/User.js
+++ models/User.js
@@ -39,21 +39,21 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// userSchema.pre('save', async function (next) {
-//   // Only hash the password if it has been modified (or is new)
-//   if (!this.isModified('password')) {
-//     return next();
-//   }
+userSchema.pre('save', async function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified('password')) {
+    return next();
+  }
 
-//   try {
-//     // Generate salt and hash password
-//     const salt = await bcrypt.genSalt(10);
-//     this.password = await bcrypt.hash(this.password, salt);
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+  try {
+    // Generate salt and hash password
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 userSchema.methods.comparePassword = async function (providedPassword) {
   return await bcrypt.compare(providedPassword, this.password);
